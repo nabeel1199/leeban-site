7e8deeaf90752d2131b9da05f1c747a19abbf30e
LeebaN <nabeel.khan3009@gmail.com>
2026-03-23 10:35:15 +0800

    init leeban site

---
+++ app/about/page.tsx
@@ -0,0 +1,11 @@
+export default function About() {
+  return (
+    <main style={{ padding: 40 }}>
+      <h1>About</h1>
+      <p>
+        Leeban Soft Dev is focused on building modern, scalable apps
+        with strong user experience and real-world utility.
+      </p>
+    </main>
+  )
+}
